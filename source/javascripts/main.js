20d97969715a8a768e0746f9a5bcb364bb92815b
Jan Vlnas <git@jan.vlnas.cz>
2014-07-03 22:47:15 +0200

    Content drawer

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,1 +1,27 @@
 //= require_tree .
+
+
+
+(function() {
+  'use strict';
+
+  var showMore = function(selector) {
+    var $expandWrapper = $('<div class="js-hide-wr"/>');
+    var $expandBtn = $('<button type="Button" class="js-hide-expander">Více</button>');
+    var classToggler = function(target) {
+      return function() {
+        $(target).toggleClass('hide');
+      };
+    };
+    $(selector).addClass('hide').each(function() {
+      var $this = $(this);
+      var $btn = $expandBtn.clone();
+      $btn.click(classToggler(this));
+      $(this).before($expandWrapper.append($btn));
+    });
+  };
+
+  showMore('.js-hide');
+
+
+})();
