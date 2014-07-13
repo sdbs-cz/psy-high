ee0e1feb98b28a1abddc10b2b87bc193787feb3b
Jan Vlnas <git@jan.vlnas.cz>
2014-07-13 18:43:29 +0200

    Lineup toggler

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,11 +1,15 @@
-//= require_tree .
-
-
-
 (function() {
   'use strict';
 
-  var showMore = function(selector) {
+
+  var forEach = function (array, callback, scope) {
+    for (var i = 0; i < array.length; i++) {
+      callback.call(scope, i, array[i]); // passes back stuff we need
+    }
+  };
+
+
+  /*var showMore = function(selector) {
     var $expandWrapper = $('<div class="js-hide-wr"/>');
     var $expandBtn = $('<button type="Button" class="js-hide-expander">Více</button>');
     var classToggler = function(target) {
@@ -21,7 +25,28 @@
     });
   };
 
-  showMore('.js-hide');
+  showMore('.js-hide');*/
+
+
+  var hideClass = 'js-hide';
+  var showClass = 'js-show';
+  var activeClass = 'js-active';
+
+  var elementToggler = function(target) {
+    return function() {
+      target.classList.toggle(hideClass);
+      target.classList.toggle(showClass);
+    };
+  };
+
+  // Line-up expander
+  var lineupItems = document.querySelectorAll('.lup-item');
+  forEach(lineupItems, function(i, el){
+    var toggler = el.querySelector('.lup-title');
+    var toggleTarget = el.querySelector('.lup-desc');
+    toggler.addEventListener('click', elementToggler(toggleTarget));
+  });
+
 
 
 })();
