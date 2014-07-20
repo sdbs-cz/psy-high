9b84174ca59115835d21b25b91c5eb8b0368f4dd
Jan Vlnas <git@jan.vlnas.cz>
2014-07-20 03:18:20 +0200

    Add helpers from headhesive to util

--- source/javascripts/util.js
+++ source/javascripts/util.js
@@ -1,24 +1,4 @@
 
-// Matches polyfill
-// https://gist.github.com/jonathantneal/3062955
-window.Element && function(ElementPrototype) {
-  ElementPrototype.matches = ElementPrototype.matches ||
-    ElementPrototype.matchesSelector ||
-    ElementPrototype.mozMatchesSelector ||
-    ElementPrototype.msMatchesSelector ||
-    ElementPrototype.oMatchesSelector ||
-    ElementPrototype.webkitMatchesSelector ||
-    function(selector) {
-      var node = this,
-        nodes = (node.parentNode || node.document).querySelectorAll(selector),
-        i = -1;
-
-      while (nodes[++i] && nodes[i] != node);
-
-      return !!nodes[i];
-    };
-}(Element.prototype);
-
 var forEach = function (array, callback, scope) {
   for (var i = 0; i < array.length; i++) {
     callback.call(scope, array[i], i); // passes back stuff we need
@@ -40,3 +20,72 @@ var toggleClasses = function(target, classes) {
   });
   return lastClass;
 };
+
+var requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
+        function(fn){window.setTimeout(fn, 15);};
+
+/**
+ * Taken from Headhesive helpers
+ */
+
+/**
+ * _mergeObj
+ * @description Merge objects
+ */
+var _mergeObj = function(to, from) {
+    for (var p in from) {
+        if (from.hasOwnProperty(p)) {
+            to[p] = (typeof from[p] === 'object') ? _mergeObj(to[p], from[p]) : from[p];
+        }
+    }
+    return to;
+};
+
+
+/**
+ * _throttle
+ * @description Borrowed from Underscore.js
+ */
+var _throttle = function(func, wait) {
+    var _now =  Date.now || function() { return new Date().getTime(); };
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    var later = function() {
+      previous = _now();
+      timeout = null;
+      result = func.apply(context, args);
+      context = args = null;
+    };
+    return function() {
+        var now = _now();
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0) {
+            clearTimeout(timeout);
+            timeout = null;
+            previous = now;
+            result = func.apply(context, args);
+            context = args = null;
+        } else if (!timeout) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result;
+    };
+};
+
+
+/**
+ * _getElemY
+ * @description Get Y posistion of an element
+ */
+function _getElemY(elem) {
+    var top = 0;
+    while(elem) {
+        top += elem.offsetTop;
+        elem = elem.offsetParent;
+    }
+    return top;
+}
