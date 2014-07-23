963e805c27119f71e621baed5adaa18c9091df57
Jan Vlnas <git@jan.vlnas.cz>
2014-07-23 23:34:23 +0200

    Add class for generated embeds

--- source/javascripts/lib/embed.js
+++ source/javascripts/lib/embed.js
@@ -1,8 +1,9 @@
 var generateEmbed = function(target) {
   var iframe = document.createElement('iframe'),
       src = target.getAttribute('data-embed');
-  iframe.setAttribute('seamless', null);
+  iframe.setAttribute('seamless', 'seamless');
+  iframe.setAttribute('class', 'js-generated-embed');
   iframe.src = src;
-  iframe.appendChild(target.cloneNode());
+  // iframe.appendChild(target.cloneNode());
   return iframe;
 };
