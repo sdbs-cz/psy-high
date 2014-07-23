4d4bce99dd2678b4cef61e14831f29f9bee6c68a
Jan Vlnas <git@jan.vlnas.cz>
2014-07-24 01:28:36 +0200

    Generate embed wrapper

--- source/javascripts/overlays.js
+++ source/javascripts/overlays.js
@@ -1,7 +1,10 @@
 
 Overlay.init();
 var overlayEmbedHandler = function(target) {
-  return generateEmbed(target).outerHTML;
+  var container = document.createElement('div');
+  container.setAttribute('class', 'overlay-inner--embed');
+  container.appendChild(generateEmbed(target));
+  return container.outerHTML;
 };
 forEach(document.querySelectorAll('.js-overlay-embed'), function(el) {
   el.addEventListener('click', Overlay.show(el, overlayEmbedHandler), false);
