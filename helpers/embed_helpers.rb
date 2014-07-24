3f104fc38e41736e6b70e186669516f88fee80b4
Jan Vlnas <git@jan.vlnas.cz>
2014-07-24 02:15:29 +0200

    Embed band videos normally

--- helpers/embed_helpers.rb
+++ helpers/embed_helpers.rb
@@ -1,13 +1,13 @@
 module EmbedHelpers
   def embed_link_for(band)
     css_class = 'js-embed'
-    overlay = false
     content = band.provider
-    if band.provider == 'YouTube'
-      overlay = true
-      css_class = 'js-overlay-embed'
-      content = image_tag(band.embed_image, alt: band.provider)
-    end
+    # overlay = false
+    # if band.provider == 'YouTube'
+    #   overlay = true
+    #   css_class = 'js-overlay-embed lup-link--img'
+    #   content = image_tag(band.embed_image, alt: band.provider)
+    # end
 
     link_to content, band.url, class: [css_class, 'lup-link'].join(' '),
       data: {embed: band.embed, height: band.embed_height }
