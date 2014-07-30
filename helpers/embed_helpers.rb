efe70d0c248a27b11610e8f3de1441ce8dbb0a51
Jan Vlnas <git@jan.vlnas.cz>
2014-07-30 19:23:27 +0200

    Bring lineup styling up to master's style

--- helpers/embed_helpers.rb
+++ helpers/embed_helpers.rb
@@ -1,6 +1,6 @@
 module EmbedHelpers
-  def embed_link_for(band)
-    css_class = 'js-embed'
+  def embed_link_for(band, css_class: '')
+    js_class = 'js-embed'
     content = band.provider
     # overlay = false
     # if band.provider == 'YouTube'
@@ -9,7 +9,7 @@ module EmbedHelpers
     #   content = image_tag(band.embed_image, alt: band.provider)
     # end
 
-    link_to content, band.url, class: [css_class, 'lup-link'].join(' '),
+    link_to content, band.url, class: [js_class, css_class].join(' '),
       data: {embed: band.embed, height: band.embed_height }
   end
 
