d5ef51cdacc966424463457e00e4fa19fb429744
Jan Vlnas <git@jan.vlnas.cz>
2014-08-05 00:03:54 +0200

    Handle active element state, embeds management

--- source/javascripts/_lineup.js
+++ source/javascripts/_lineup.js
@@ -3,28 +3,34 @@
 
 
 var ActiveElementState = function() {
-  var _activeElement;
+  var _state = new State();
+  var _oldValue;
 
-  return function(newElement) {
-    toggleActiveElement(_activeElement, false);
-    _activeElement = newElement;
-    toggleActiveElement(newElement, true);
+  return {
+    set: function(value) {
+      _oldValue = _state.get();
+      _state.set(value);
+    },
+    transition: function(callback) {
+      _state.watch(function(newValue) {
+        callback(_oldValue, newValue);
+      });
+    }
+
   };
+
 };
 
 var LineupExpander = function(rootEl) {
   var DEFAULT_STATE = '#lineup';
   var SEL_ITEM = '.lup-item',
     SEL_CLICK_TGT = '.lup-l',
-    SEL_DETAIL = '.lup-det',
-    SEL_DETAIL_TARGETS = '.js-det-tgt',
     SEL_EMBED_LINK = '.js-embed';
 
   var _root = rootEl;
   var _state = new State(window.location.hash);
+  var _activeElement = ActiveElementState();
 
-
-  var setActiveElement = ActiveElementState();
 
   // Always push location hash on state change
   _state.watch(function(hsh){
@@ -32,22 +38,61 @@ var LineupExpander = function(rootEl) {
   });
 
   _state.when(DEFAULT_STATE, function(){
-    setActiveElement(null);
+    _activeElement.set(null);
   });
 
-  forEach(_root.querySelectorAll(SEL_ITEM), function(item){
+  _activeElement.transition(function(oldElement, newElement) {
+    if(oldElement) {
+      toggleActiveElement(oldElement, false);
+      unloadEmbed(oldElement);
+    }
 
+    if(newElement) {
+      convertEmbed(newElement) || reloadEmbed(newElement);
+      toggleActiveElement(newElement, true);
+    }
+  });
+
+
+  var unloadEmbed = function(parent) {
+    var iframe = parent.querySelector('iframe');
+    if(iframe) {
+      unloadIframe(iframe);
+    }
+  };
+
+  var reloadEmbed = function(parent) {
+    var iframe = parent.querySelector('iframe');
+    if(iframe) {
+      reloadIframe(iframe);
+      return true;
+    }
+    return false;
+  };
+
+  var convertEmbed = function(parent) {
+    var elink = parent.querySelector(SEL_EMBED_LINK);
+    if(elink) {
+      var embed = generateEmbed(elink);
+      elink.parentNode.replaceChild(embed, elink);
+      return true;
+    }
+    return false;
+  };
+
+  forEach(_root.querySelectorAll(SEL_ITEM), function(item){
     var id = item.id;
     var clickTarget = item.querySelector(SEL_CLICK_TGT);
 
     _state.when('#' + id, function(){
-      setActiveElement(item);
+      _activeElement.set(item);
       smoothScroll(item, 500, null, 'up');
     });
 
 
     clickTarget.addEventListener('click', function(ev){
       ev.preventDefault();
+      this.blur();
       if(this.hash === window.location.hash) {
         _state.set(DEFAULT_STATE);
       }
@@ -57,78 +102,6 @@ var LineupExpander = function(rootEl) {
     }, false);
   });
 
-    // SEL_DETAIL_COPY = '.lup-det--';
-    /*
-  var _activeItem = new State();
-  var _activeDetails = new State();
-
-  var nextClosestSibling = function(forId, nthName) {
-    var siblingSel = '#' + forId + ' ~ .lup-det--' + nthName;
-    return document.querySelector(siblingSel);
-  };
-
-  var closestSiblings = function(forEl) {
-    var id = forEl.id;
-    var siblings = [
-      nextClosestSibling(id, '2n'),
-      nextClosestSibling(id, '3n')
-    ];
-    // Remove null
-    return siblings.filter(function(e) {
-      return e !== null;
-    });
-  };
-
-  var copyContents = function(from, to) {
-    forEach(to, function(target) {
-      target.innerHTML = from.innerHTML;
-    });
-  };
-
-  var convertEmbeds = function(parent) {
-    var elink = parent.querySelector(SEL_EMBED_LINK);
-    if(elink) {
-      var embed = generateEmbed(elink);
-      elink.parentNode.replaceChild(embed, elink);
-    }
-  };
-
-  // click handlers and state observer for individual items
-  forEach(_root.querySelectorAll(SEL_ITEM), function(item){
-    var clickTarget = item.querySelector(SEL_CLICK_TGT),
-        localDetail = item.querySelector(SEL_DETAIL),
-        copyTargets = closestSiblings(item);
-    // var hideTargets = copyTargets.concat(localDetail);
-
-    clickTarget.addEventListener('click', function(){
-      if(isActiveElement(item)) {
-        _activeItem.set(null);
-        _activeDetails.set([]);
-      } else {
-        _activeItem.set(item);
-      }
-    });
-
-    _activeItem.watch(function(selected){
-      var isThis = (selected === item);
-
-      toggleActiveElement(item, isThis);
-      if(isThis) {
-        convertEmbeds(localDetail);
-        copyContents(localDetail, copyTargets);
-        _activeDetails.set(copyTargets);
-        smoothScroll(item, 500, null, 'up');
-      }
-    });
-  });
-
-  // state observers for details
-  forEach(_root.querySelectorAll(SEL_DETAIL_TARGETS), function(self){
-    _activeDetails.watch(function(activeGrp){
-      var isSelf = (activeGrp.indexOf(self) !== -1);
-      toggleElement(self, isSelf);
-    });
-  });*/
 };
 
 forEach(document.querySelectorAll('.js-lup-wr'), function(lupWr) {
