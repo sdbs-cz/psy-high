00763f2f0fa61269aa7bf44bfba858ce1677bd5f
Jan Vlnas <git@jan.vlnas.cz>
2014-08-05 19:04:42 +0200

    Improve Bandcamp scraping
    Calculate embed's height based on number of tracks,
    show playlist only for albums with 2 and more tracks.

--- lib/embeds.rb
+++ lib/embeds.rb
@@ -6,6 +6,62 @@ Oembedr.configure do |configuration|
   configuration.adapter = :excon # :typhoeus seems broken
 end
 
+class BandcampEmbed
+  TRACK_HEIGHT = 32
+  BASE_HEIGHT = 120
+
+  MAX_HEIGHT = 500
+  EMBED_SIZE = 'large'
+
+  def initialize(url)
+    @src_doc = Nokogiri::HTML(Excon.get(url).body)
+    @src_url = @src_doc.at('meta[property="twitter:player"]').attr('content')
+  end
+
+  def embed
+    if player_id.blank?
+      nil
+    else
+      "//bandcamp.com/EmbeddedPlayer/#{player_id}/size=large/bgcol=fafafa/linkcol=27CDFC/artwork=small/transparent=true/#{tracklist_param}"
+    end
+  end
+
+  def embed_height
+    if track?
+      BASE_HEIGHT
+    else
+      length = tracks_length + 1 # due to Bandcamp's bottom padding
+      [MAX_HEIGHT, BASE_HEIGHT + TRACK_HEIGHT * length].min
+    end
+  end
+
+  private
+
+  def tracklist_param
+    if track?
+      'tracklist=false/'
+    else
+      ''
+    end
+  end
+
+  def track?
+    player_id.include?('track=') || tracks_length == 1
+  end
+
+  def album?
+    player_id.include?('album=')
+  end
+
+  def tracks_length
+    @tracks_length = @src_doc.css('[itemprop=tracks]').length
+  end
+
+  def player_id
+    @player_id ||= @src_url[/((album|track)=[^\/]+)/i, 1]
+  end
+end
+
 class Embed
   attr_reader :url, :embed, :provider, :embed_image, :embed_height
 
@@ -36,18 +92,9 @@ class Embed
 
   private
   def fetch_bandcamp(url)
-    doc = Nokogiri::HTML(Excon.get(url).body)
-    embedurl = doc.at('meta[property="twitter:player"]').attr('content')
-    @embed = bandcamp_url(embedurl[/((album|track)=[^\/]+)/i, 1])
-    @embed_height = doc.at('meta[property="twitter:player:height"]').attr('content')
-  end
-
-  def bandcamp_url(id)
-    if id.blank?
-      nil
-    else
-      "//bandcamp.com/EmbeddedPlayer/#{id}/size=large/bgcol=fafafa/linkcol=27CDFC/artwork=small/transparent=true/"
-    end
+    bc = BandcampEmbed.new(url)
+    @embed = bc.embed
+    @embed_height = bc.embed_height
   end
 
   def fetch_oembed(url)
