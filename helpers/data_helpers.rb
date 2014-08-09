09b29cab3cccb85ba5ac246ed9aaa9ad1f38556f
Jan Vlnas <git@jan.vlnas.cz>
2014-08-09 20:39:19 +0200

    Add td data helper

--- helpers/data_helpers.rb
+++ helpers/data_helpers.rb
@@ -2,4 +2,12 @@ module DataHelpers
   def ldata
     data[I18n.locale]
   end
+
+  def td parent, key
+    if parent.send("#{key}?")
+      parent[key]
+    else
+      parent["#{key}_#{I18n.locale}"]
+    end
+  end
 end
