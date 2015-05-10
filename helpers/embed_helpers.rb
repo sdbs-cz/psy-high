7b2d798dc7e0da8a58a85489dfc09f1dcf6890f5
Jan Vlnas <git@jan.vlnas.cz>
2015-05-10 17:28:52 -0500

    Roll youtube embeds to a helper

--- helpers/embed_helpers.rb
+++ helpers/embed_helpers.rb
@@ -13,5 +13,14 @@ module EmbedHelpers
       data: {embed: band.embed, height: band.embed_height }
   end
 
+  def youtube_embed(id, css_class = '')
+    js_class = 'js-overlay-embed'
+    link_url = "https://www.youtube.com/watch?v=#{id}"
+    embed_url = "//www.youtube-nocookie.com/embed/#{id}?rel=0&autoplay=1&color=white&showinfo=1&disablekb=1&html5=1"
+    link_to link_url, class: [js_class, css_class].join(' '), data: {embed: embed_url} do
+      yield
+    end
+  end
+
 
 end
