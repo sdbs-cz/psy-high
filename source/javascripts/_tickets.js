858a6905d1e1870f3f11d78e3a93d3e61bf3f42a
Jan Vlnas <git@jan.vlnas.cz>
2015-05-13 23:17:22 -0500

    Check if tickets are not sold out

--- source/javascripts/_tickets.js
+++ source/javascripts/_tickets.js
@@ -1,27 +1,34 @@
+(function(){
 
-var TicketsForm = function(form) {
+var CLS_FLASH_OK = 'flash-success',
+    CLS_FLASH_ERROR = 'flash-error',
+    API_BASE = 'http://progrestinate.com/ticketpro',
+    LOW_COUNT = 50;
+
+var setFlashMessage = function(flashContainer, state) {
+  var flashContainer,
+      cssClass = CLS_FLASH_OK,
+      msg;
+
+  if(state !== 'ok') {
+    cssClass = CLS_FLASH_ERROR;
+  }
+  flashContainer.classList.add(cssClass);
+  msg = flashContainer.getAttribute('data-' + state);
+  flashContainer.innerHTML = msg;
+
+  showElement(flashContainer);
+};
+
+var TicketsForm = function(form, flashElm) {
 
   var _form = form;
 
   var _submitBtn = _form.querySelector('.js-submit');
 
-  var CLS_FLASH_OK = 'flash-success',
-      CLS_FLASH_ERROR = 'flash-error';
-
   var setFlash = function(state) {
-    var flashContainer = _form.querySelector('.js-flash'),
-        cssClass = CLS_FLASH_OK,
-        msg;
-
-    if(state !== 'ok') {
-      cssClass = CLS_FLASH_ERROR;
-    }
-    flashContainer.classList.add(cssClass);
-    msg = flashContainer.getAttribute('data-' + state);
-    flashContainer.innerHTML = msg;
-
-    showElement(flashContainer);
-  };
+    return setFlashMessage(flashElm, state);
+  }
 
   var responseLoad = function() {
     var data = JSON.parse(this.responseText);
@@ -98,4 +105,36 @@ var TicketsForm = function(form) {
   _form.addEventListener('submit', sendForm, false);
 };
 
-TicketsForm(document.getElementById('tickets-f'));
+function ticketsCount(cb) {
+  var ENDPOINT = API_BASE + '/ticketCounter.php';
+
+  var xhr = new XMLHttpRequest();
+  xhr.onerror = function() {
+    cb(this.statusText);
+  };
+  xhr.onload = function() {
+    var data = JSON.parse(this.responseText);
+    var tickets = data.available;
+    cb(null, tickets);
+  };
+  xhr.open('GET', ENDPOINT, true);
+  //.setRequestHeader("Content-type","application/x-www-form-urlencoded")
+  xhr.send();
+}
+
+var $form = document.getElementById('tickets-f');
+var $fieldset = document.getElementById('tickets-f-set');
+var $flashElm = $form.querySelector('.js-flash');
+
+// On load: Hit API server to check if any tickets are available
+ticketsCount(function(err, result) {
+  if(result > 0) {
+    TicketsForm($form, $flashElm);
+  }
+  else {
+    $fieldset.setAttribute('disabled', true);
+    setFlashMessage($flashElm, 'soldout');
+  }
+})
+
+}());
