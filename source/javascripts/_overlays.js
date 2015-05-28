0d3cad2a66978389574977c425080c0efbe06e04
Tomáš Mládek <tomas0mladek@gmail.com>
2015-05-28 16:26:54 +0200

    add rudimentary overlay for location images

--- source/javascripts/_overlays.js
+++ source/javascripts/_overlays.js
@@ -9,3 +9,16 @@ var overlayEmbedHandler = function(target) {
 forEach(document.querySelectorAll('.js-overlay-embed'), function(el) {
   el.addEventListener('click', Overlay.show(el, overlayEmbedHandler), false);
 });
+
+var overlayImageHandler = function(target) {
+  var image = document.createElement('img');
+  image.setAttribute('src', target.getAttribute('href'));
+  image.setAttribute('class', 'js-generated-image');
+  var container = document.createElement('div');
+  container.setAttribute('class', 'overlay-inner--embed');
+  container.appendChild(image);
+  return container.outerHTML;
+};
+forEach(document.querySelectorAll('.js-overlay-image'), function(el) {
+  el.addEventListener('click', Overlay.show(el, overlayImageHandler), false);
+});
