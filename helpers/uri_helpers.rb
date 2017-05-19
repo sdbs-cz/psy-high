2cbf46c4b86bd4321272407188d3b3d0a9410e33
Jan Vlnas <git@jan.vlnas.cz>
2017-05-19 15:55:38 +0200

    Fix URI helpers

--- helpers/uri_helpers.rb
+++ helpers/uri_helpers.rb
@@ -1,12 +1,12 @@
 module UriHelpers
   def cname
-    settings[:cname] || I18n.t(:cname)
+    config[:cname] || I18n.t(:cname)
   end
 
   def canonical(path = nil)
     unless path.start_with?('/')
       path = '/' + path
     end
-    URI::HTTP.build(host: cname, path: build_root + path)
+    URI::HTTP.build(host: cname, path: config[:build_root] + path)
   end
 end
