9cac9a0ba77d630a496c7d5f0287dd0e963ae8bf
Tomáš Mládek <tmladek@inventati.org>
2017-06-21 20:34:39 +0200

    do not load video on mobile

--- source/javascripts/_video.js
+++ source/javascripts/_video.js
@@ -1,13 +1,17 @@
-var adaptVideo = function(){
-  var vid = document.getElementById("bgvid");
-  if (window.screen.width / window.screen.height > 1920/1080){
-    vid.style.height = "auto";
-    vid.style.width = "100%";
-  } else {
-    vid.style.height = "100%";
-    vid.style.width = "auto";
+var vid = document.getElementById("bgvid");
+if (window.mobilecheck()){
+  vid.outerHTML = ""; 
+} else {
+  var adaptVideo = function(){
+    if (window.screen.width / window.screen.height > 1920/1080){
+      vid.style.height = "auto";
+      vid.style.width = "100%";
+    } else {
+      vid.style.height = "100%";
+      vid.style.width = "auto";
+    }
   }
+  
+  window.addEventListener("resize", adaptVideo);
+  adaptVideo();
 }
-
-window.addEventListener("resize", adaptVideo);
-adaptVideo();
