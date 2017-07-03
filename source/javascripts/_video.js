2072d06aea21a0cbf7e27f0882f2ba4de8b40d71
Tomáš Mládek <tmladek@inventati.org>
2017-07-03 13:27:29 +0200

    fix ratio calculation / video full

--- source/javascripts/_video.js
+++ source/javascripts/_video.js
@@ -1,9 +1,10 @@
+var wrap = document.getElementById("header-bgvid");
 var vid = document.getElementById("bgvid");
 if (window.mobilecheck()){
   vid.outerHTML = ""; 
 } else {
   var adaptVideo = function(){
-    if (window.screen.width / window.screen.height > 1920/1080){
+    if (wrap.offsetWidth / wrap.offsetHeight > 1920/1080){
       vid.style.height = "auto";
       vid.style.width = "100%";
     } else {
